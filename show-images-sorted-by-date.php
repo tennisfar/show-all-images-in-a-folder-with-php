a229e6052494610109a79d1fa6f740c30f3fe200
@slowzone <slow.zone.mail@gmail.com>
2014-07-25 22:19:45 +0200

    add files

---
+++ show-images-sorted-by-date.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html">
+<title>Show images in folder</title>
+<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+ 
+<?php
+date_default_timezone_set("Europe/Copenhagen");
+$folder = 'img/';
+$filetype = '*.*';
+$files = glob($folder.$filetype);
+$count = count($files);
+ 
+$sortedArray = array();
+for ($i = 0; $i < $count; $i++) {
+    $sortedArray[date ('YmdHis', filemtime($files[$i])) . $i] = $files[$i];
+}
+ 
+ksort($sortedArray);
+# krsort($sortedArray);
+
+echo '<table>';
+foreach ($sortedArray as &$filename) {
+    echo '<tr><td>';
+    echo '<a name="'.$filename.'" href="#'.$filename.'"><img src="'.$filename.'" /></a>';
+    echo substr($filename,strlen($folder),strpos($filename, '.')-strlen($folder));
+    echo '</td></tr>';
+}
+echo '</table>';
+?>
